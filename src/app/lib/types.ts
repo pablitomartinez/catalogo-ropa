b00ee35e60190133c09562f9b9b3e49bd64d60a6
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2024-12-03 11:07:37 -0300

    feat: configuracion de tipos para la aplicacino

--- src/app/lib/types.ts
+++ src/app/lib/types.ts
@@ -1,1 +1,28 @@
 //definicion de tipos
+// src/lib/types.ts
+
+export interface Clothing {
+  id: string;
+  name: string;
+  description: string;
+  shortDescription: string;
+  images: string[];
+  sizes: string[];
+  inStock: boolean;
+  stockQuantity: number;
+  price: number;
+  category: 'tops' | 'bottoms' | 'dresses' | 'accessories';
+}
+
+export interface ClothingFormData extends Omit<Clothing, 'id'> {
+  id?: string;
+}
+
+export interface AuthSession {
+  user: {
+    id: string;
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+  };
+}
